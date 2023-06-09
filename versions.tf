d5fdf415eeca8f49e5d90a8db4beaf8e529c157c
Saurabh <saurabhdevle@gmail.com>
2023-06-09 16:37:01 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "SmithaOrg"
+    organization = "TerraformTestSaurabh"
 
     workspaces {
       name = "sentinel-example"
